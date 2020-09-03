4699bfb9d03e35be6e8c3ee913b6807a9330f2e9
Yuri Albuquerque <yuridenommus@gmail.com>
2020-09-03 20:49:14 -0300

    Initial commit

---
+++ global/nixos/configuration.nix
@@ -0,0 +1,139 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+let
+  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+    export __NV_PRIME_RENDER_OFFLOAD=1
+    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    export __VK_LAYER_NV_optimus=NVIDIA_only
+    exec -a "$0" "$@"
+  '';
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.supportedFilesystems = [ "ntfs" ];
+
+  networking.hostName = "yuri-nixos"; # Define your hostname.
+  networking.networkmanager.enable = true;
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.enp2s0.useDHCP = true;
+  networking.interfaces.wlp3s0.useDHCP = true;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  environment.systemPackages = with pkgs; [
+    firefox
+    emacs
+    mu
+    git
+    pciutils
+    glxinfo
+    kdeApplications.spectacle
+    nvidia-offload
+    iotop
+  ];
+
+  console.useXkbConfig = true;
+  # Select internationalisation properties.
+  i18n.defaultLocale = "pt_BR.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  # };
+
+  # Set your time zone.
+  time.timeZone = "America/Sao_Paulo";
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  #   pinentryFlavor = "gnome3";
+  };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.layout = "br";
+  services.xserver.xkbOptions = "ctrl:nocaps";
+
+  # Enable touchpad support.
+  services.xserver.libinput.enable = true;
+
+  # Enable the KDE Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.yuri = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ];
+    shell = pkgs.zsh;
+  };
+
+  programs.zsh.enable = true;
+
+  nixpkgs.config.allowUnfree = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.03"; # Did you read the comment?
+
+  hardware.opengl.driSupport32Bit = true;
+  hardware.opengl.enable = true;
+  hardware.opengl.extraPackages32 = [ pkgs.pkgsi686Linux.libva ];
+  hardware.nvidia.prime = {
+    offload.enable = true;
+    nvidiaBusId = "PCI:1:0:0";
+    intelBusId = "PCI:0:2:0";
+  };
+  hardware.nvidia.modesetting.enable = true;
+  hardware.nvidia.nvidiaPersistenced = true;
+
+  services.xserver.videoDrivers = [ "nvidia" "modesetting" ];
+  services.xserver.dpi = 96;
+
+  nix.autoOptimiseStore = true;
+}
+
---
+++ home/nixpkgs/home.nix
@@ -0,0 +1,64 @@
+{ config, pkgs, ... }:
+
+{
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "20.03";
+
+  home.sessionVariables = {
+    EDITOR = "emacsclient";
+  };
+
+  home.packages = with pkgs; [
+    keepassxc
+    tdesktop
+    spotify
+    steam
+    rustup
+    discord
+    signal-desktop
+    nodejs
+    aspell
+    aspellDicts.pt_BR
+    aspellDicts.en
+  ];
+
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    history.extended = true;
+    oh-my-zsh = {
+      enable = true;
+      theme = "mortalscumbag";
+    };
+  };
+
+  programs.git = {
+    enable = true;
+    ignores = [ "*~" ];
+    lfs.enable = true;
+    userEmail = "yuridenommus@gmail.com";
+    userName = "Yuri Albuquerque";
+  };
+
+  programs.emacs.enable = true;
+
+  services.emacs.enable = true;
+  services.emacs.client.enable = true;
+
+  services.dropbox.enable = true;
+
+
+  programs.zsh.shellAliases = {
+    vim = "emacsclient -t ";
+  };
+}
