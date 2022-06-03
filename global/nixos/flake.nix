5b7192cb67c42a07135d3271bfecf49a63c8046b
Yuri Albuquerque <yuridenommus@gmail.com>
2022-06-03 01:28:43 -0300

    Start to move nix-darwin-configurations to this repo

--- global/nixos/flake.nix
+++ global/nixos/flake.nix
@@ -5,33 +5,62 @@
     nur.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    #home-manager.inputs.nur.follows = "nur";
+    nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-21.11-darwin";
+    darwin.url = "github:lnl7/nix-darwin/master";
+    darwin.inputs.nixpkgs.follows = "nixpkgs-darwin";
   };
 
-  outputs = inputs: let nur-no-pkgs = import inputs.nur {
-    pkgs = null;
-    nurpkgs = import inputs.nixpkgs {
-      system = "x86_64-linux";
+  outputs = { self, nixpkgs, nur, home-manager, nixpkgs-darwin, darwin }@inputs:
+  let
+    nur-no-pkgs = import nur {
+      pkgs = null;
+      nurpkgs = import nixpkgs {
+        system = "x86_64-linux";
+      };
     };
-  };
   in {
-    nixosConfigurations.yuri-nixos = inputs.nixpkgs.lib.nixosSystem {
+    nixosConfigurations.yuri-nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [
         ./configuration.nix
-        { nixpkgs.overlays = [ inputs.nur.overlay ]; }
-        inputs.home-manager.nixosModules.home-manager (let
+        { nixpkgs.overlays = [ nur.overlay ]; }
+        home-manager.nixosModules.home-manager (let
         in {
           home-manager = {
             useGlobalPkgs = true;
             useUserPackages = true;
             users.yuri = {
-	      imports = [
-	        ./yuri/home.nix
-		nur-no-pkgs.repos.rycee.hmModules.emacs-init
-	      ];
-	    };
+	            imports = [
+	              ./yuri/home.nix
+		            nur-no-pkgs.repos.rycee.hmModules.emacs-init
+	            ];
+	          };
+          };
+        })
+      ];
+    };
+
+    darwinConfigurations."Yuris-MacBook-Pro" = darwin.lib.darwinSystem {
+      system = "x86_64-darwin";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./darwin-configuration.nix
+        { nixpkgs.overlays = [ nur.overlay ]; }
+        home-manager.darwinModules.home-manager ({
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            extraSpecialArgs = {
+              unstable-pkgs = nixpkgs.legacyPackages.x86_64-darwin;
+              system = "x86_64-darwin";
+            };
+            users.yurialbuquerque = {
+              imports = [
+                ./yurialbuquerque/home.nix
+                nur-no-pkgs.repos.rycee.hmModules.emacs-init
+              ];
+            };
           };
         })
       ];
