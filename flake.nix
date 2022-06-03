c2124c9b23e7345d0046d0851beef7d46c70ba5b
Yuri Albuquerque <yuridenommus@gmail.com>
2022-06-03 11:23:35 -0300

    Fix nur on Darwin

--- flake.nix
+++ flake.nix
@@ -1,4 +1,6 @@
 {
+  description = "Yuri's Nix system configurations";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nur.url = "github:nix-community/NUR";
@@ -8,14 +10,22 @@
     nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-21.11-darwin";
     darwin.url = "github:lnl7/nix-darwin/master";
     darwin.inputs.nixpkgs.follows = "nixpkgs-darwin";
+    nur-darwin.url = "github:nix-community/NUR";
+    nur-darwin.inputs.nixpkgs.follows = "nixpkgs-darwin";
   };
 
-  outputs = { self, nixpkgs, nur, home-manager, nixpkgs-darwin, darwin }@inputs:
+  outputs = { self, nixpkgs, nur, home-manager, nixpkgs-darwin, darwin, nur-darwin }@inputs:
   let
     nur-no-pkgs = import nur {
       pkgs = null;
       nurpkgs = import nixpkgs {
         system = "x86_64-linux";
+      };
+    };
+    nur-darwin-no-pkgs = import nur-darwin {
+      pkgs = null;
+      nurpkgs = import nixpkgs-darwin {
+        system = "x86_64-darwin";
       };
     };
   in {
@@ -46,7 +56,7 @@
       specialArgs = { inherit inputs; };
       modules = [
         ./darwin-configuration.nix
-        { nixpkgs.overlays = [ nur.overlay ]; }
+        { nixpkgs.overlays = [ nur-darwin.overlay ]; }
         home-manager.darwinModules.home-manager ({
           home-manager = {
             useGlobalPkgs = true;
@@ -58,7 +68,7 @@
             users.yurialbuquerque = {
               imports = [
                 ./darwin/yurialbuquerque/home.nix
-                nur-no-pkgs.repos.rycee.hmModules.emacs-init
+                nur-darwin-no-pkgs.repos.rycee.hmModules.emacs-init
               ];
             };
           };
