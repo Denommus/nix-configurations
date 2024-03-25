528428453968487df4acc515fcfa69bab6e51c2d
Yuri Albuquerque <yuridenommus@gmail.com>
2024-03-25 14:12:24 -0300

    Update flake and update name of autosuggestion option on zsh

--- shared/home.nix
+++ shared/home.nix
@@ -12,7 +12,7 @@
 
   programs.zsh = {
     enable = true;
-    enableAutosuggestions = true;
+    autosuggestion.enable = true;
     enableCompletion = true;
     syntaxHighlighting.enable = true;
     history.extended = true;
