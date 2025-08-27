73d47a67e5e77ea7b9719ecfad6e449141398893
Yuri Albuquerque <yuridenommus@gmail.com>
2025-08-27 16:26:07 -0300

    nix-darwin now needs stateVersion = 5

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -34,7 +34,7 @@
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
-  system.stateVersion = 4;
+  system.stateVersion = 5;
   nix = {
     enable = true;
     optimise.automatic = true;
