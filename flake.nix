7f350a578769f7b8a1124d7f62ce641c362d062b
Yuri Albuquerque <yuridenommus@gmail.com>
2026-02-06 14:48:34 -0300

    Install nix-rosetta-builder

--- flake.nix
+++ flake.nix
@@ -9,70 +9,94 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     darwin.url = "github:lnl7/nix-darwin/master";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-rosetta-builder = {
+      url = "github:cpick/nix-rosetta-builder";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, nur, home-manager, darwin, ... }@inputs:
-  let
-    nur-no-pkgs = (system: import nur {
-      pkgs = null;
-      nurpkgs = import nixpkgs {
-        inherit system;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nur,
+      home-manager,
+      darwin,
+      nix-rosetta-builder,
+      ...
+    }@inputs:
+    let
+      nur-no-pkgs = (
+        system:
+        import nur {
+          pkgs = null;
+          nurpkgs = import nixpkgs {
+            inherit system;
+          };
+        }
+      );
+    in
+    {
+      nixosConfigurations.yuri-nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./configuration.nix
+          {
+            nixpkgs.overlays = [
+              (nur.overlays.default)
+              (final: prev: {
+                wrap-wine = (import ./linux/wine-packages/wrap-wine.nix { pkgs = final; });
+              })
+              (final: prev: {
+                hero-lab = final.callPackage ./linux/wine-packages/hero-lab.nix { };
+              })
+            ];
+          }
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.yuri = {
+                imports = [
+                  ./linux/yuri/home.nix
+                  (nur-no-pkgs "x86_64-linux").repos.rycee.hmModules.emacs-init
+                ];
+              };
+            };
+          }
+        ];
       };
-    });
-  in {
-    nixosConfigurations.yuri-nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = { inherit inputs; };
-      modules = [
-        ./configuration.nix
-        {
-          nixpkgs.overlays = [
-            (nur.overlays.default)
-            (final: prev: {
-              wrap-wine = (import ./linux/wine-packages/wrap-wine.nix { pkgs = final; });
-            })
-            (final: prev: {
-              hero-lab = final.callPackage ./linux/wine-packages/hero-lab.nix {};
-            })
-          ];
-        }
-        home-manager.nixosModules.home-manager {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            users.yuri = {
-	            imports = [
-	              ./linux/yuri/home.nix
-		            (nur-no-pkgs "x86_64-linux").repos.rycee.hmModules.emacs-init
-	            ];
-	          };
-          };
-        }
-      ];
-    };
 
-    darwinConfigurations.MacBook-Pro-de-Yuri = darwin.lib.darwinSystem {
-      system = "aarch64-darwin";
-      specialArgs = { inherit inputs; };
-      modules = [
-        ./darwin-configuration.nix
-        { nixpkgs.overlays = [ nur.overlays.default ]; }
-        home-manager.darwinModules.home-manager {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            extraSpecialArgs = {
-              system = "aarch64-darwin";
+      darwinConfigurations.MacBook-Pro-de-Yuri = darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./darwin-configuration.nix
+          { nixpkgs.overlays = [ nur.overlays.default ]; }
+          home-manager.darwinModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              extraSpecialArgs = {
+                system = "aarch64-darwin";
+              };
+              users.yuri = {
+                imports = [
+                  ./darwin/yurialbuquerque/home.nix
+                  (nur-no-pkgs "aarch64-darwin").repos.rycee.hmModules.emacs-init
+                ];
+              };
             };
-            users.yuri = {
-              imports = [
-                ./darwin/yurialbuquerque/home.nix
-                (nur-no-pkgs "aarch64-darwin").repos.rycee.hmModules.emacs-init
-              ];
-            };
-          };
-        }
-      ];
+          }
+          # { nix.linux-builder.enable = true; }
+          nix-rosetta-builder.darwinModules.default
+          {
+            nix-rosetta-builder.onDemand = true;
+          }
+        ];
+      };
     };
-  };
 }
